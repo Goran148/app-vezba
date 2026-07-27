9a14d76b501aa51b642cdddac3986478dccb52d4
MacBook Air <macbookair@Gorans-MacBook-Air.local>
2026-07-27 17:57:29 +0200

    dodat docker

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify(
+        {
+            "status": "success",
+            "message": "DevOps projekat uspešno radi!",
+            "environment": os.getenv("ENV", "development"),
+        }
+    )
+
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
