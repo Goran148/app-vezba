0e03fd62aee652a6b1f8fe0ea485dce1a4d1fa25
MacBook Air <macbookair@192.168.1.8>
2026-07-28 12:01:49 +0200

    test_branch

---
+++ terraform/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-east-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-east-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "App-Vezba"
+
+  instance_type = "t3.micro"
+  key_name      = "user1"
+  monitoring    = true
+  subnet_id     = module.vpc.private_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "App-Vezba-SG"
+  description = "Example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules = {
+    https = {
+      from_port   = 5000
+      ip_protocol = "tcp"
+      cidr_ipv4   = "10.0.0.0/16"
+      description = "HTTPS from internal"
+    }
+    self-all = {
+      ip_protocol                  = "-1"
+      referenced_security_group_id = "self"
+      description                  = "All traffic from members of this SG"
+    }
+  }
+
+  egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
